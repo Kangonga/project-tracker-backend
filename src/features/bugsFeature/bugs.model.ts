a7fd3c49fc0ea1a42c8f48af3b04b3efd1c31189
Kangonga <guchu413@gmail.com>
2023-06-03 05:54:24 +0300

    feat: create the bugs model

---
+++ src/features/bugsFeature/bugs.model.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from 'mongoose';
+
+import { Bug } from '@app/features/bugsFeature/bugs.interface';
+
+const bugSchema = new Schema<Bug>({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  priority: { type: String, required: true },
+  status: { type: String, required: true },
+  reportedById: { type: String, required: true },
+  assigneeId: { type: String, required: true },
+  closedById: { type: String, required: true },
+  createdAt: { type: String, required: true },
+  closedAt: { type: String, required: true },
+});
+
+const BugModel = model<Bug>('Bug', bugSchema);
+
+export default BugModel;
