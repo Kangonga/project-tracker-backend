1bb6e0cad4a10b4883dd3884e2cd1e1bfac15966
Kangonga <guchu413@gmail.com>
2023-06-04 07:47:58 +0300

    feat: create user controllers and services

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,26 +1,35 @@
-import { Request, Response } from 'express';
+import {
+  createOneUser,
+  deleteSingleUser,
+  findAllUsers,
+  findUser,
+  findUserById,
+  updateSingleUser,
+} from '@app/services/user.service';
+import { modelAsyncWrapper } from '@app/wrappers/trycatch';
+import { NextFunction, Request, Response } from 'express';
 
-export const getAllUsers = async (req: Request, res: Response) => {
-  try {
-  } catch (error) {}
-};
+export const getAllUsers = modelAsyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
+  const { users } = await findAllUsers();
+  return res.status(200).json({ data: users });
+});
 
-export const createUser = async (req: Request, res: Response) => {
-  try {
-  } catch (error) {}
-};
+export const createUser = modelAsyncWrapper(async (req: Request, res: Response) => {
+  const { newuser } = await createOneUser(req.body);
+  return res.status(200).json(newuser);
+});
 
-export const getUserById = async (req: Request, res: Response) => {
-  try {
-  } catch (error) {}
-};
+export const getUserById = modelAsyncWrapper(async (req: Request, res: Response) => {
+  const { user } = await findUser('title', String(req.params.id));
+  return res.status(200).json(user);
+});
 
 export const updateUserById = async (req: Request, res: Response) => {
-  try {
-  } catch (error) {}
+  const { updatedUser } = await updateSingleUser(String(req.params.id), req.body);
+  return res.status(200).json(updatedUser);
 };
 
 export const deleteUserById = async (req: Request, res: Response) => {
-  try {
-  } catch (error) {}
+  const { deletedUser } = await deleteSingleUser(String(req.params.id));
+  return res.status(200).json({ msg: 'User deleted successfuly' });
 };
---
+++ src/services/user.service.ts
@@ -0,0 +1,56 @@
+import { User } from '@app/interfaces/user.interface';
+import UserModel from '@app/models/user.model';
+
+export const findAllUsers = async () => {
+  let error = false;
+  const users = await UserModel.find();
+  if (!users) {
+    error = true;
+  }
+  return { error, users };
+};
+
+export const createOneUser = async (user: User) => {
+  const newuser = await UserModel.create(user);
+  if (!(newuser instanceof UserModel)) {
+    throw Error('User was not created');
+  }
+  return { newuser };
+};
+
+export const findUserById = async (id: string) => {
+  const user = await UserModel.findOne({ _id: id });
+  if (!user) {
+    throw Error('User not found');
+  }
+  return { user };
+};
+
+export const findUser = async (field: string, value: string) => {
+  const user = await UserModel.findOne({ field: value });
+  if (!user) {
+    throw Error('User not found');
+  }
+  return { user };
+};
+
+export const updateSingleUser = async (userID: string, user: User) => {
+  const filter = { _id: userID };
+  const updatedUser = await UserModel.findOneAndUpdate(filter, user, {
+    new: true,
+    runValidators: true,
+  });
+  if (!updatedUser) {
+    throw Error('User not found');
+  }
+  return { updatedUser };
+};
+
+export const deleteSingleUser = async (userID: string) => {
+  const filter = { _id: userID };
+  const deletedUser = await UserModel.findOneAndDelete(filter);
+  if (!deletedUser) {
+    throw Error('User not found');
+  }
+  return { deletedUser };
+};
