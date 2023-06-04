360ab78f7b130e48616aedae51133d5ec9147080
Kangonga <guchu413@gmail.com>
2023-06-04 08:13:27 +0300

    feat: add password interface and schema validation

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -6,6 +6,7 @@ const userSchema = new Schema<User>({
   lastName: { type: String, required: true },
   email: { type: String, required: true },
   role: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 const UserModel = model<User>('User', userSchema);
