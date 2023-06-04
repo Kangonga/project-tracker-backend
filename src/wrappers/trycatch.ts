b15d919a23d0e73a0220f082b7a24e5634273bc6
Kangonga <guchu413@gmail.com>
2023-06-04 07:30:21 +0300

    feat: create async wrapper for the model crud operations

---
+++ src/wrappers/trycatch.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+import { MongooseError } from 'mongoose';
+
+interface callbackInterface {
+  (req: Request, res: Response, next: NextFunction): Promise<Response>;
+}
+export const modelAsyncWrapper = (callback: callbackInterface) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await callback(req, res, next);
+    } catch (error) {
+      if (error instanceof MongooseError) {
+        return res.status(404).json({ err: error.name });
+      }
+      if (error instanceof Error) {
+        return res.status(404).json({ err: error.message });
+      }
+      res.status(404).json({ err: error });
+    }
+  };
+};
