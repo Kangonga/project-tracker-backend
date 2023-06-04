71ec73c3a1cafabc82637e921d9665331e9a8dfc
Kangonga <guchu413@gmail.com>
2023-06-04 22:40:21 +0300

    feat: add sync wrapper to the sign in controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
 
 import { findAndAuthenticateUser } from '@app/services/auth services/findAndAuthenticateUser';
+import { modelAsyncWrapper } from '@app/wrappers/modelAsyncWrapper';
 
-export const signIn = async (req: Request, res: Response) => {
+export const signIn = modelAsyncWrapper(async (req: Request, res: Response) => {
   const { email, password } = req.body;
   const user = await findAndAuthenticateUser(email, password);
-  res.json({ msg: 'authenticate user', user });
-};
+  return res.json({ msg: 'user authenticated', user });
+});
