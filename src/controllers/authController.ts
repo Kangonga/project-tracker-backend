04bc867b64f8fae2375334053cd4a7e13a8fbd32
Kangonga <guchu413@gmail.com>
2023-06-05 03:12:46 +0300

    feat: add sign out controller and session creation in the sign in route

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -6,5 +6,13 @@ import { modelAsyncWrapper } from '@app/wrappers/modelAsyncWrapper';
 export const signIn = modelAsyncWrapper(async (req: Request, res: Response) => {
   const { email, password } = req.body;
   const user = await findAndAuthenticateUser(email, password);
-  return res.json({ msg: 'user authenticated', user });
+  req.session.isAuth = true;
+  return res.status(200).json({ msg: 'user authenticated', user, session: req.session, sessionID: req.session.id });
+});
+
+export const signOut = modelAsyncWrapper(async (req: Request, res: Response) => {
+  req.session.destroy(err => {
+    if (err) throw Error(err);
+  });
+  return res.status(200).json({ msg: 'success. user logged out' });
 });
