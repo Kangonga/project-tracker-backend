c93dae08e0b546976cf9348998bb4711971c3cc9
Kangonga <guchu413@gmail.com>
2023-06-05 10:23:27 +0300

    feat: add the find and delete session service

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import { findAndAuthenticateUser } from '@app/services/auth services/findAndAuthenticateUser';
 import { modelAsyncWrapper } from '@app/wrappers/modelAsyncWrapper';
 import { createOneUser } from '@app/services/crud operations/user.service';
+import { findAndDeleteSession } from '@app/services/auth services/findAndDeleteSessions';
 
 export const register = modelAsyncWrapper(async (req: Request, res: Response) => {
   const { newuser } = await createOneUser(req.body);
@@ -12,8 +13,9 @@ export const register = modelAsyncWrapper(async (req: Request, res: Response) =>
 export const signIn = modelAsyncWrapper(async (req: Request, res: Response) => {
   const { email, password } = req.body;
   const user = await findAndAuthenticateUser(email, password);
+  await findAndDeleteSession(user._id.toString());
   req.session.isAuth = true;
-  req.session.userId = user._id;
+  req.session.userId = user._id.toString();
   return res.status(200).json({ msg: 'user authenticated', user, session: req.session, sessionID: req.session.id });
 });
 
