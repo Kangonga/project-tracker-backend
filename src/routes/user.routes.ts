c96c4a65e573d0e2ad7139c2961f0521430831dc
Kangonga <guchu413@gmail.com>
2023-06-05 05:59:06 +0300

    feat: add session verification middleware to user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 
 import { createUser, deleteUserById, getAllUsers, getUserById, updateUserById } from '@app/controllers/user.controller';
+import { verifySessionExists } from '@app/middleware/verifySessionExists';
 
 const userRoutes = Router();
+
+userRoutes.use(verifySessionExists);
 
 userRoutes.route('/').get(getAllUsers).post(createUser);
 userRoutes.route('/:id').get(getUserById).patch(updateUserById).delete(deleteUserById);
