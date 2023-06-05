c15e3e76290dcda20c242747d0e95fe1d32d954d
Kangonga <guchu413@gmail.com>
2023-06-05 13:30:12 +0300

    feat: create verify user roles to the user create and delete controllers

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,12 +2,13 @@ import { Router } from 'express';
 
 import { createUser, deleteUserById, getAllUsers, getUserById, updateUserById } from '@app/controllers/user.controller';
 import { verifySessionExists } from '@app/middleware/verifySessionExists';
+import { verifyUserRoles } from '@app/middleware/verifyUserRoles';
 
 const userRoutes = Router();
 
 userRoutes.use(verifySessionExists);
 
-userRoutes.route('/').get(getAllUsers).post(createUser);
-userRoutes.route('/:id').get(getUserById).patch(updateUserById).delete(deleteUserById);
+userRoutes.route('/').get(getAllUsers).post(verifyUserRoles, createUser);
+userRoutes.route('/:id').get(getUserById).patch(updateUserById).delete(verifyUserRoles, deleteUserById);
 
 export default userRoutes;
