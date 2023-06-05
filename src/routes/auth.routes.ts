036f54f14e3624290f5636400f7b33ebe0e59933
Kangonga <guchu413@gmail.com>
2023-06-05 03:12:31 +0300

    feat: add sign out route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
-import { signIn } from '@app/controllers/authController';
+import { signIn, signOut } from '@app/controllers/authController';
 
 const authRoutes = Router();
 
 authRoutes.route('/signin').post(signIn);
+authRoutes.route('/signout').post(signOut);
 
 export default authRoutes;
