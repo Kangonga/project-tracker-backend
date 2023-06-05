5bdf3d30b98e647ef31ff7f51249ab787824df3f
Kangonga <guchu413@gmail.com>
2023-06-05 13:29:19 +0300

    feat: add verify user roles middlewre to the projects routes

--- src/routes/project.routes.ts
+++ src/routes/project.routes.ts
@@ -8,10 +8,11 @@ import {
   updateProjectById,
 } from '@app/controllers/project.controller';
 import { verifySessionExists } from '@app/middleware/verifySessionExists';
+import { verifyUserRoles } from '@app/middleware/verifyUserRoles';
 
 const projectRoutes = Router();
 
-projectRoutes.use(verifySessionExists);
+projectRoutes.use([verifySessionExists, verifyUserRoles]);
 
 projectRoutes.route('/').get(getAllProjects).post(createProject);
 projectRoutes.route('/:id').get(getProjectById).patch(updateProjectById).delete(deleteProjectById);
