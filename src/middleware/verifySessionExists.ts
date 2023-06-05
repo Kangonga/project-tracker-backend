0a5567282e828299beb43283572d95de2597d591
Kangonga <guchu413@gmail.com>
2023-06-05 06:22:33 +0300

    refactor: user id instead of session id in verification of a session

--- src/middleware/verifySessionExists.ts
+++ src/middleware/verifySessionExists.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 
 export const verifySessionExists = async (req: Request, res: Response, next: NextFunction) => {
-  const sessionId = req.sessionID;
-  if (!sessionId) {
+  const userId = req.session.userId;
+  if (!userId) {
     return res.status(401).json({ err: 'Unauthorized. Invalid session' });
   }
   next();
