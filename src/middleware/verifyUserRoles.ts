4b02f7d38480a192441fc04f7ef4f7277c8b5c9b
Kangonga <guchu413@gmail.com>
2023-06-08 07:18:29 +0300

    feat: wrap the verifyUserRoles middleware with the async wrapper

--- src/middleware/verifyUserRoles.ts
+++ src/middleware/verifyUserRoles.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
 
 import { findUserById } from '@app/services/crud operations/user.service';
+import { middlewareAsyncWrapper } from '@app/wrappers/middlewareAyncWrapper';
 
-export const verifyUserRoles = async (req: Request, res: Response, next: NextFunction) => {
-  const { user } = await findUserById(req.params.userId);
+export const verifyUserRoles = middlewareAsyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
+  const { user } = await findUserById(req.session.userId as string);
   if (user.role !== 'admin') {
     throw Error('Unauthorized: Invalid permissions');
   }
   next();
-};
+});
