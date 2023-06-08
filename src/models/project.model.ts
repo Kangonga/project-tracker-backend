dda9f0a36cd24ddf997421eb04951282efaa03ee
Kangonga <guchu413@gmail.com>
2023-06-08 08:15:45 +0300

    feat: add unique validaor and insert date pre save hook

--- src/models/project.model.ts
+++ src/models/project.model.ts
@@ -1,10 +1,16 @@
 import { Schema, model } from 'mongoose';
+import mongooseUniqueValidator from 'mongoose-unique-validator';
 
 import { Project } from '@app/interfaces/project.interface';
+import { insertDate } from '@app/services/insertDate';
 
 const projectSchema = new Schema<Project>({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
+  createdAt: { type: String },
 });
+
+projectSchema.plugin(mongooseUniqueValidator);
+projectSchema.pre('save', insertDate);
 
 const ProjectModel = model<Project>('Project', projectSchema);
 
