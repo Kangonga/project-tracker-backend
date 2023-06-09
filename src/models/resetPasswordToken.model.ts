02d36d74150a2e0455f420518cfeb0823720e38c
Kangonga <guchu413@gmail.com>
2023-06-09 11:34:43 +0300

    refactor: change the document expiration time

--- src/models/resetPasswordToken.model.ts
+++ src/models/resetPasswordToken.model.ts
@@ -5,7 +5,7 @@ import { resetPasswordTokenInterface } from '@app/interfaces/resetPasswordToken.
 const resetPasswordTokenSchema = new Schema<resetPasswordTokenInterface>({
   token: { type: String },
   email: { type: String, required: true },
-  expireAt: { type: Date, expires: 5, default: Date.now },
+  expireAt: { type: Date, expires: 600, default: Date.now },
 });
 
 const ResetPasswordTokenModel = model<resetPasswordTokenInterface>('PasswordToken', resetPasswordTokenSchema);
