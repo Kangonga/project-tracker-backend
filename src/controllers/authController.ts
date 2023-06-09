396d729c8bc74e5a76361fee2d6d35b901e23852
Kangonga <guchu413@gmail.com>
2023-06-09 05:20:51 +0300

    feat: create the reset password request, verify token and reset password controllers

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,8 +2,11 @@ import { Request, Response } from 'express';
 
 import { findAndAuthenticateUser } from '@app/services/auth services/findAndAuthenticateUser';
 import { modelAsyncWrapper } from '@app/wrappers/modelAsyncWrapper';
-import { createOneUser } from '@app/services/crud operations/user.service';
+import { createOneUser, findUser, findUserById } from '@app/services/crud operations/user.service';
 import { findAndDeleteSession } from '@app/services/auth services/findAndDeleteSessions';
+import { sendEmail } from '@app/services/auth services/sendEmail';
+import { createToken } from '@app/services/auth services/createToken';
+import { findAndVerifyToken } from '@app/services/auth services/findAndVerifyToken';
 
 export const register = modelAsyncWrapper(async (req: Request, res: Response) => {
   const { newuser } = await createOneUser(req.body);
@@ -25,4 +28,30 @@ export const signOut = modelAsyncWrapper(async (req: Request, res: Response) => 
   });
   res.clearCookie('connect.sid');
   return res.status(200).json({ msg: 'success. user logged out' });
+});
+
+export const resetPasswordRequest = modelAsyncWrapper(async (req: Request, res: Response) => {
+  const { email } = req.body;
+  await findUser('email', email);
+  const { newToken } = await createToken(email);
+  sendEmail(email, newToken.token);
+  return res.status(200).json({ msg: 'token sent to the provided email' });
+});
+
+export const verifyToken = modelAsyncWrapper(async (req: Request, res: Response) => {
+  const { token, email } = req.body;
+  findAndVerifyToken(token, email);
+  const { user } = await findUser('email', email);
+  req.session.userId = user._id;
+  return res.status(200).json({ msg: 'valid token' });
+});
+
+export const resetPassword = modelAsyncWrapper(async (req: Request, res: Response) => {
+  const { password, confirmPassword } = req.body;
+  if (password !== confirmPassword) {
+    throw Error('Passwords do not match');
+  }
+  const { user } = await findUserById(req.session.userId as string);
+  user.password = password;
+  return res.status(200).json({ msg: 'success. password updated' });
 });
