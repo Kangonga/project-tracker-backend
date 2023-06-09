68ddf9aea04e41e4edb23f1b5a009f13cc4a4106
Kangonga <guchu413@gmail.com>
2023-06-09 08:40:50 +0300

    feat: refactor the code and export the unhashed token

--- src/services/auth services/createToken.ts
+++ src/services/auth services/createToken.ts
@@ -1,11 +1,12 @@
+import { randomUUID } from 'crypto';
+
 import { hash } from 'bcrypt';
 
 import ResetPasswordTokenModel from '@app/models/resetPasswordToken.model';
-import { randomUUID } from 'crypto';
 
 export const createToken = async (email: string) => {
   const token = randomUUID().toString().substring(0, 5);
   const hashedToken = await hash(token, 10);
   const newToken = await ResetPasswordTokenModel.create({ email, token: hashedToken });
-  return { newToken, hashedToken };
+  return { newToken, hashedToken, token };
 };
