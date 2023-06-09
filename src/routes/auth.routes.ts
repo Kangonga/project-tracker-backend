7abfc69a25c707d9f7ce3b30171853324b263faf
Kangonga <guchu413@gmail.com>
2023-06-09 05:52:31 +0300

    feat: add a new route to verify the token. and add session verification middleware

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 
-import { register, resetPasswordRequest, signIn, signOut } from '@app/controllers/authController';
+import {
+  register,
+  resetPassword,
+  resetPasswordRequest,
+  signIn,
+  signOut,
+  verifyToken,
+} from '@app/controllers/authController';
+import { verifySessionExists } from '@app/middleware/verifySessionExists';
 
 const authRoutes = Router();
 
 authRoutes.route('/register').post(register);
 authRoutes.route('/forgotPassword').post(resetPasswordRequest);
+authRoutes.route('/verifyToken').post(verifySessionExists, verifyToken);
+authRoutes.route('/resetPassword').post(verifySessionExists, resetPassword);
 authRoutes.route('/signin').post(signIn);
 authRoutes.route('/signout').post(signOut);
 
