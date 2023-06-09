3d7a2e4622b249c21c1b86ce220692510bc60d2e
Kangonga <guchu413@gmail.com>
2023-06-09 05:21:50 +0300

    feat: create the forgot password route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import { register, signIn, signOut } from '@app/controllers/authController';
+import { register, resetPasswordRequest, signIn, signOut } from '@app/controllers/authController';
 
 const authRoutes = Router();
 
 authRoutes.route('/register').post(register);
+authRoutes.route('/forgotPassword').post(resetPasswordRequest);
 authRoutes.route('/signin').post(signIn);
 authRoutes.route('/signout').post(signOut);
 
